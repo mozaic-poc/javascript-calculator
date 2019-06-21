e98141405aef2af9bf988bdc30df832b8873263e
Robert Kent <rob.kent@mozaic.net>
2019-06-21 15:50:52 +0100

    Mark uploads as publically accessible

--- calculator-deploy.sh
+++ calculator-deploy.sh
@@ -49,4 +49,4 @@ if [ x"$UPDATE_ONLY" = x'false' ]; then
 	aws cloudformation wait stack-create-complete --stack-name "$STACK_NAME"
 fi
 
-aws s3 sync --delete . "s3://$BucketName/" --exclude '.git/*' --exclude .gitignore --exclude Jenkinsfile --exclude calculator-deploy.sh --exclude parameters.json
+aws s3 sync --acl public-read --delete . "s3://$BucketName/" --exclude '.git/*' --exclude .gitignore --exclude Jenkinsfile --exclude calculator-deploy.sh --exclude parameters.json
